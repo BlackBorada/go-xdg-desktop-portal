445e18dae2cdc1741def976288baaf80dba64af2
BlackBorada <BlackBorada1@yandex.ru>
2024-11-19 20:57:58 +0500

    API expanded

--- api.go
+++ api.go
@@ -7,6 +7,40 @@ const (
 
 	AccountInterface = "org.freedesktop.portal.Account"
 
+	BackgroundInterface = "org.freedesktop.portal.Background"
+
+	CameraInterface = "org.freedesktop.portal.Camera"
+
+	ClipboardInterface = "org.freedesktop.portal.Clipboard"
+
+	DocumentsInterface = "org.freedesktop.portal.Documents"
+
+	DynamicLauncherInterface = "org.freedesktop.portal.DynamicLauncher"
+
+	EmailInterface = "org.freedesktop.portal.Email"
+
+	FileChooserInterface = "org.freedesktop.portal.FileChooser"
+
+	FileTransferInterface = "org.freedesktop.portal.FileTransfer"
+
+	GameModeInterface = "org.freedesktop.portal.GameMode"
+
+	GlobalShortcutsInterface = "org.freedesktop.portal.GlobalShortcuts"
+
+	InhibitInterface = "org.freedesktop.portal.Inhibit"
+
+	InputCaptureInterface = "org.freedesktop.portal.InputCapture"
+
+	LocationInterface = "org.freedesktop.portal.Location"
+
+	MemoryMonitorInterface = "org.freedesktop.portal.MemoryMonitor"
+
+	NetworkMonitorInterface = "org.freedesktop.portal.NetworkMonitor"
+
+	NotificationInterface = "org.freedesktop.portal.Notification"
+
+	OpenURIInterface = "org.freedesktop.portal.OpenURI"
+
 	ScreenCastInterface = "org.freedesktop.portal.ScreenCast"
 
 	RequestInterface = "org.freedesktop.portal.Request"
@@ -17,8 +51,3 @@ const (
 
 	PortalImpl = "org.freedesktop.impl.portal"
 )
-
-type SessionOptions struct {
-	HandleToken        string
-	SessionHandleToken string
-}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/BlackBorada/go-xdg-desktop-portal
 
 go 1.23.2
 
-require github.com/godbus/dbus/v5 v5.1.0 // indirect
+require github.com/godbus/dbus/v5 v5.1.0
